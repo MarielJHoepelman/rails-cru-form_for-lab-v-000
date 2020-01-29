f3acfed44522f0ce4ec29ecc63084d272579c66f
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-29 23:00:44 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,35 @@
 class GenresController < ApplicationController
+  # before_action :find_artist, only: [:show, :edit, :update]
+
+ def new
+   @genre = Genre.new
+ end
+
+ def create
+   @genre = Genre.create(name: params[:genre][:name])
+   redirect_to artist_path(@genre)
+ end
+
+
+ def show
+   @genre = Genre.find(params[:id])
+ end
+ #
+ # def edit
+ # end
+ #
+ # def update
+ #   @artist.update(post_params)
+ #   redirect_to artist_path(@artist)
+ # end
+
+ private
+
+ # def find_artist
+ #   @artist = Artist.find(params[:id])
+ # end
+ #
+ # def post_params
+ #   params.require(:artist).permit(:name, :bio)
+ # end
 end
