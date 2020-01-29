3828af3f4bda99c30d707e05909ff6b53e4f014b
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-01-29 22:45:12 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,34 @@
 class ArtistsController < ApplicationController
+  # before_action :find_student, only: [:show, :edit, :update]
+
+ def new
+   @artist = Artist.new
+ end
+
+ def create
+   @student = Student.create(post_params)
+   redirect_to student_path(@student)
+ end
+
+
+ def show
+ end
+
+ def edit
+ end
+
+ def update
+   @student.update(post_params)
+   redirect_to student_path(@student)
+ end
+
+ private
+ #
+ # def find_student
+ #   @student = Student.find(params[:id])
+ # end
+ #
+ # def post_params
+ #   params.require(:student).permit(:first_name, :last_name)
+ # end
 end
